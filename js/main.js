3d4c06a0fd334a2c336bccdf66d15e03dcec5ee2
luizfspinto <luizfspintoo@gmail.com>
2023-03-20 14:10:49 -0300

    active menu effect

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-//Abre e fecha o menu
+//Abre e fecha o menu no mobile
 const nav = window.document.querySelector("#header nav")
 const toggle = window.document.querySelectorAll("nav .toggle")
 
-//para cada elemento com toggle
 for (const element of toggle) {
   element.addEventListener("click", function () {
     nav.classList.toggle("show")
   })
 }
 
-//Quando clicar em um dos links do menu, esconde o menu
+//Quando clicar em um dos links do menu, esconde o menu, no mobile
 const links = window.document.querySelectorAll("nav ul li a")
 for (const link of links) {
   link.addEventListener("click", function () {
@@ -17,29 +16,55 @@ for (const link of links) {
   })
 }
 
-/*====== SCROLL ======= */
+/*====== efeito no cabeçalho ao rolar a página ======= */
+const header = window.document.querySelector("#header")
+const navHeight = header.offsetHeight
 function moveScrollHeader() {
-  const header = window.document.querySelector("#header")
-  const navHeight = header.offsetHeight
   if (window.scrollY >= navHeight) {
     header.classList.add("scroll")
   } else {
-    //Menor que a altura do header
     header.classList.remove("scroll")
   }
 }
 
+/*efeito de aparecer o botão ao rolar a página*/
+const backToTopButton = window.document.querySelector(".back-to-top")
 function moveBackToTop() {
-  const backToTopButton = window.document.querySelector('.back-to-top')
-  if(window.scrollY >= 560) {
-    backToTopButton.classList.add('show')
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add("show")
   } else {
-    backToTopButton.classList.remove('show')
+    backToTopButton.classList.remove("show")
   }
 }
 
-/*invocation functions*/
+/*efeito de aparecer link e divisor no botão de menu, ao rolar a página*/
+const sections = window.document.querySelectorAll("main section[id]")
+function activeMenuSection() {
+  var checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop //topo da seção
+    const sectionHeight = section.offsetHeight //altura da seção
+    const sectionId = section.getAttribute("id") //identificador
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.add("active")
+    } else {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.remove("active")
+    }
+  }
+}
+
+/*invocações das funções*/
 window.addEventListener("scroll", function () {
   moveScrollHeader()
-   moveBackToTop()
+  moveBackToTop()
+  activeMenuSection()
 })
